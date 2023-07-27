12f2582a1775702dfa6a6e716d688aa404167de2
Aaro Ylimaula <aaro.ylimaula@aalto.fi>
2023-07-27 14:21:05 +0300

    5.1

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')   
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [newTitle, setNewTitle ] = useState('')
+  const [newAuthor, setNewAuthor ] = useState('')
+  const [newUrl, setNewUrl ] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try{
+      const user = await loginService.login({
+        username, password
+      })
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }catch(exception){
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input 
+            type='text' 
+            value={username}
+            name='Username'
+            onChange={({target}) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        password
+          <input
+            type='text'
+            value={password}
+            name='Password'
+            onChange={({target}) => setPassword(target.value)}
+          />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+  
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      Title
+      <input
+        value={newTitle}
+        onChange={handleTitleChange}
+      />
+      <br></br>
+      Author
+      <input
+        value={newAuthor}
+        onChange={handleAuthorChange}
+      />
+      <br></br>
+      Url
+      <input
+        value={newUrl}
+        onChange={handleUrlChange}
+      />
+      <br></br>
+      <button type='submit'>save</button>
+    </form>
+  )
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification message={errorMessage} />
+
+      {!user && loginForm()} 
+      {user && <div>
+        <p>{user.name} logged in</p>
+          {blogForm()}
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
+      
+      
+    </div>
+  )
+}
+
+export default App
